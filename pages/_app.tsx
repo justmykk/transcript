95ffa1344a80592a9cc78d309753775c8da79dd4
Michael Olawale <aymikey.olawale@gmail.com>
2021-02-26 18:00:18 +0100

    web title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+        <title>Transcript</title>
       </Head>
       <Component {...pageProps} />
     </>
