5e6be7a419239c454bb2aa0a069fdd2f6a73cebb
Michael Olawale <aymikey.olawale@gmail.com>
2021-03-23 09:59:36 +0100

    school fees

--- pages/school-fees.tsx
+++ pages/school-fees.tsx
@@ -1,10 +1,40 @@
+import { FormEvent } from 'react';
 import { CommonAppBar } from '../components/CommonAppBar';
 import { CommonLayout } from '../components/CommonLayout';
 
 const SchoolFees = () => {
+  const onSubmit = (ev: FormEvent) => {
+    ev.preventDefault();
+  };
+
   return (
     <CommonLayout>
-      <h1>SchoolFees</h1>
+      <div className="schoolfees">
+        <img src="/school_fees.svg" alt="under construction" />
+
+        <div className="schoolfees__container">
+          <p className="schoolfees__title">Coming Soon</p>
+          <p className="schoolfees__subtitle">
+            Working on some awesome{' '}
+            <span className="schoolfees__subtitle--highlight">
+              School Fees Payment
+            </span>{' '}
+            features.
+          </p>
+
+          <form className="schoolfees__form" onSubmit={onSubmit}>
+            <input placeholder="Your Email" required type="email" />
+
+            <button type="submit">
+              <img src="/button_arrow.svg" alt="button arrow" />
+            </button>
+          </form>
+
+          <p className="schoolfees__notify">
+            You will get notify when this feature is available
+          </p>
+        </div>
+      </div>
     </CommonLayout>
   );
 };
